bb48d474060a8e7283aaadf6c0a7a0dc597fa460
Jorgealmaraz0204 <141947952+Jorgealmaraz0204@users.noreply.github.com>
2023-08-12 23:18:28 -0600

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -104,29 +104,3 @@ case 2:
 
 
 
-void ALGORITMOBASICO()
-{
-int Primo(int numero);
-
-int main(void) {
-  int numero;
-  printf("Dime un número para saber si es un mumero primo o no: \n");
-  scanf("%d", &numero);
-  if (Primo(numero)) {
-    printf("VERDADERO");
-  } else {
-    printf("FALSO");
-  }
-  return 0;
-}
-
-int Primo(int numero) {
-  if (numero == 0 || numero == 1) return 0;
- 
-  if (numero == 4) return 0;
-  for (int x = 2; x < numero / 2; x++) {
-    
-    if (numero % x == 0) return 0;
-  }
-  return 1;
-}
