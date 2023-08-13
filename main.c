545dd98316d23c77bb237e175cecf39d1269894a
Jorgealmaraz0204 <141947952+Jorgealmaraz0204@users.noreply.github.com>
2023-08-12 23:30:54 -0600

    Update main.c

--- main.c
+++ main.c
@@ -8,10 +8,47 @@ int main(void)
 {
 
   
-void ALGORITMOBASICO();
-void LOOPS();
-void VECTORES();
-void CALCULADORA();
+
+
 
   return 0;
+  do{
+        printf("MENU\n");
+        printf("1. loops \n");
+        printf("2. calculadora\n");
+        printf("3. numero primo \n");
+        printf("4. calificaciones\n");
+        printf("5. vectores\n");
+        
+        printf("Seleccione una opcion: ");
+	scanf("%d", &opcion);
+	
+	switch (opcion)
+       {
+       		case 1: 
+         void LOOPS();
+       			break;
+case 2: 
+       void CALCULADORA();
+       			break;
+       			
+       		case 3: 
+       			void numeroprimo();
+       			break;
+       			
+       		case 4: 
+      void calificaciones();
+       			break;
+
+        		case 4: 
+      void vectores();
+       			break;
+       		default:
+			printf("Opción invalida \n");
+			break;
+       }
+    }while(opcion != 6);
+
+    return 0;
+}
 }
